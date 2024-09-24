dc431eb79ab44cc721b8874623982736275fc5c7
amaralfatah <amaralfatah.main@gmail.com>
2024-09-24 23:11:54 +0700

    fix route

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-portfolio/'  // Ganti dengan nama repository GitHub kamu
+    ? '/portfolio-vue/'  // Ganti dengan nama repository GitHub kamu
     : '/' // local
 })
